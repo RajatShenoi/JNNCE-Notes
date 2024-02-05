f6757b236e61e1b018fffa90fa4a692ef91d5a11
Rajat Shenoi <rajatshenoi@outlook.com>
2024-02-05 21:55:11 +0530

    basic stuff complete

--- jnncenotes/urls.py
+++ jnncenotes/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # Include the notes app's URLs
+    path("", include("notes.urls")),
 ]
